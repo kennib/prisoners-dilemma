475b0c71d477cb68af30713c955ed7ec56bb2c8d
Kenni Bawden <kenni.bawden@nicta.com.au>
2014-04-30 10:03:05 +1000

    Added a tit for tat strategy

--- Prisoners.hs
+++ Prisoners.hs
@@ -1,10 +1,24 @@
 data Decision = Betray | Silent deriving (Show, Eq)
 data Strategy = AlwaysBetray
               | AlwaysSilent
+              | TitForTat
+
+justBetrayed :: [Decision] -> Bool
+justBetrayed ds
+  | null ds     = False
+  | d == Betray = True
+  | otherwise   = False
+  where d = head ds
 
 decide :: Strategy -> [Decision] -> Decision
 decide AlwaysBetray _ = Betray
 decide AlwaysSilent _ = Silent
+decide TitForTat ds
+  | justBetrayed ds = Betray
+  | otherwise       = Silent
 
 main = do
   print $ decide AlwaysBetray []
+  print $ decide TitForTat []
+  print $ decide TitForTat [Betray, Silent]
+  print $ decide TitForTat [Silent, Betray, Silent]
