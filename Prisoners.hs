c8bcb278f4ef8603728e47b04c76f1ea52c82de2
Kenni Bawden <kenni.bawden@nicta.com.au>
2014-04-30 10:45:58 +1000

    Added the start of a strategy guesser

--- Prisoners.hs
+++ Prisoners.hs
@@ -1,13 +1,19 @@
+import Data.Maybe
+
 data Decision = Betray | Silent deriving (Show, Eq)
 data Strategy = AlwaysBetray
               | AlwaysSilent
               | TitForTat
+              deriving (Show)
+
+isBetray :: Decision -> Bool
+isBetray d = (d == Betray)
 
 justBetrayed :: [Decision] -> Bool
 justBetrayed ds
-  | null ds     = False
-  | d == Betray = True
-  | otherwise   = False
+  | null ds    = False
+  | isBetray d = True
+  | otherwise  = False
   where d = head ds
 
 decide :: Strategy -> [Decision] -> Decision
@@ -17,8 +23,16 @@ decide TitForTat ds
   | justBetrayed ds = Betray
   | otherwise       = Silent
 
+guessStrategy :: [Decision] -> [Decision] -> Maybe Strategy
+guessStrategy opponents mine
+  | all isBetray opponents = Just AlwaysBetray
+  | otherwise = Nothing
+
 main = do
   print $ decide AlwaysBetray []
   print $ decide TitForTat []
   print $ decide TitForTat [Betray, Silent]
   print $ decide TitForTat [Silent, Betray, Silent]
+
+  print $ guessStrategy [Betray, Betray, Betray, Betray] [Betray, Silent, Silent, Silent]
+  print $ guessStrategy [Silent, Silent, Betray, Betray] [Silent, Betray, Betray, Betray]
